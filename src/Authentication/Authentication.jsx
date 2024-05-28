50d87b6ca2e38423a5a871c4c9e4b9e046eb8e38
kiranmogal <mogalkiransep3@gmail.com>
2024-05-28 22:50:03 +0530

    .

--- src/Authentication/Authentication.jsx
+++ src/Authentication/Authentication.jsx
@@ -6,8 +6,23 @@ function Authentication() {
     <>
       <div className="auth-top"></div>
       <div className="auth-bottom">
-        Meta About Blog Jobs Help
-        <p></p>
+        <span>Meta</span>
+        <span>About</span>
+        <span>Blog</span>
+        <span>Jobs</span>
+        <span>Help</span>
+        <span>Meta</span>
+        <span>Meta</span>
+        <span>Meta</span>
+        <span>Meta</span>
+        <span>Meta</span>
+        <span>Meta</span>
+        <span>Meta</span>
+        Jobs Help Privacy Terms Locations Instagram Lite Threads Contact
+        uploading and non-users Meta Verified
+        <p>
+          <button>English(UK)</button>© 2024 Instagram from Meta
+        </p>
       </div>
     </>
   );
